9741b3d63aa710d91ad1012c2b31e31f20705362
Samwelrangili <samwelrangili896@gmail.com>
2020-02-18 12:09:08 +0300

    added a function in script.js file

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+  });
+
+  jQuery('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  jQuery('img').click(function(){
+    alert("This is an image")
+  });
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
